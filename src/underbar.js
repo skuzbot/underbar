ccc8925862a9f6273ad9dab97671223a52fc5012
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-03 23:28:48 -0500

    Fix .filter Tests

--- src/underbar.js
+++ src/underbar.js
@@ -77,6 +77,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(value, index) {
+      if (test(value) === true) {
+        result.push(value);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
